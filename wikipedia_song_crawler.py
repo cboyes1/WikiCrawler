37ed01d73cf10c0c7ee5d0aa5a54ce433535ecce
Cameron Boyes <cboyes11@gmail.com>
2020-04-05 19:41:56 -0400

    Working Crawler

--- wikipedia_song_crawler.py
+++ wikipedia_song_crawler.py
@@ -1,6 +1,7 @@
 import os
 import time
 import random
+import requests
 from bs4 import BeautifulSoup
 
 
@@ -20,9 +21,17 @@ SONG_LIST = []
 START_YEAR = 1946
 
 while START_YEAR <= 2019:
-    time.sleep(random.randint(5, 10))
-    URL_ID = "https://en.wikipedia.org/wiki/Billboard_year-end_top_singles_of_"
-    SOUP = BeautifulSoup(URL_ID + str(START_YEAR), 'lxml')
+    if START_YEAR <= 1948:
+        URL_ID = "https://en.wikipedia.org/wiki/Billboard_year-end_top_singles_of_"
+    if 1949 <= START_YEAR <= 1955:
+        URL_ID = "https://en.wikipedia.org/wiki/Billboard_year-end_top_30_singles_of_"
+    if 1956 <= START_YEAR <= 1958:
+        URL_ID = "https://en.wikipedia.org/wiki/Billboard_year-end_top_50_singles_of_"
+    if START_YEAR >= 1959:
+        URL_ID = "https://en.wikipedia.org/wiki/Billboard_Year-End_Hot_100_singles_of_"
+
+    SOURCE = requests.get(URL_ID + str(START_YEAR)).text
+    SOUP = BeautifulSoup(SOURCE, 'lxml')
     HTML_TEXT = SOUP.find_all('td')
 
     SONG_LIST.append("Billboards Top Selling Songs For The Year: " + str(START_YEAR))
@@ -31,16 +40,20 @@ while START_YEAR <= 2019:
     COUNTER = 0
     for td in SOUP.find_all("td"):
         line = td.text
-        if not line[0].isdigit() and line[0] == "\"":
-            COUNTER += 1
-            SONG_LIST.append(str(COUNTER) + ": " + line)
-        elif not line[0].isdigit():
-            SONG_LIST.append("    " + line)
+        if len(line) >= 1:
+            if not line[0].isdigit() and line[0] == "\"":
+                COUNTER += 1
+                SONG_LIST.append(str(COUNTER) + ": " + line)
+            elif not line[0].isdigit():
+                SONG_LIST.append("    " + line)
     for i in range(2):
         SONG_LIST.append("")
+    time.sleep(random.randint(3, 7))
     START_YEAR += 1
 
 
-with open(os.path.join(HERE, 'song_titles.txt'), 'a') as file:
+
+with open(os.path.join(HERE, 'song_titles.txt'), 'w', encoding="utf-8") as file:
     for string in SONG_LIST:
-        file.write(string + "\n")
+        if not string.isdigit():
+            file.write(string + "\n")
