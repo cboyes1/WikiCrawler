c5b400b6ddf54a8981f5c772b7e6b40452b2eedb
Cameron Boyes <cboyes11@gmail.com>
2020-04-05 18:20:51 -0400

    About to test the program live, yee haw

--- wikipedia_song_crawler.py
+++ wikipedia_song_crawler.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 from bs4 import BeautifulSoup
 
 
@@ -16,25 +17,30 @@ except FileExistsError:
     pass
 
 SONG_LIST = []
-# START_YEAR = 1946
+START_YEAR = 1946
 
-# while START_YEAR <=2019 :
-with open(str(os.path.join(HERE, 'test.html')), encoding="utf8") as html_file:
-    SOUP = BeautifulSoup(html_file, 'lxml')
-
+while START_YEAR <= 2019:
+    time.sleep(random.randint(5, 10))
+    URL_ID = "https://en.wikipedia.org/wiki/Billboard_year-end_top_singles_of_"
+    SOUP = BeautifulSoup(URL_ID + str(START_YEAR), 'lxml')
     HTML_TEXT = SOUP.find_all('td')
 
+    SONG_LIST.append("Billboards Top Selling Songs For The Year: " + str(START_YEAR))
+    print("Current Year = " + str(START_YEAR))
 
-COUNTER = 0
-for td in SOUP.find_all("td"):
-    line = td.text
-    if not line[0].isdigit() and line[0] == "\"":
-        COUNTER += 1
-        SONG_LIST.append(str(COUNTER) + ": " + line)
-    elif not line[0].isdigit():
-        SONG_LIST.append("    " + line)
+    COUNTER = 0
+    for td in SOUP.find_all("td"):
+        line = td.text
+        if not line[0].isdigit() and line[0] == "\"":
+            COUNTER += 1
+            SONG_LIST.append(str(COUNTER) + ": " + line)
+        elif not line[0].isdigit():
+            SONG_LIST.append("    " + line)
+    for i in range(2):
+        SONG_LIST.append("")
+    START_YEAR += 1
 
 
-with open(os.path.join(HERE, 'song_titles.txt'), 'w') as file:
+with open(os.path.join(HERE, 'song_titles.txt'), 'a') as file:
     for string in SONG_LIST:
         file.write(string + "\n")
